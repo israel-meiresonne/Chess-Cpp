1f424c607a40a6b4a2f2a0ae8f7370d1796f4b12
Israel Meiresonne <israelmeiresonne97@gmail.com>
2025-02-15 21:57:51 +0000

    Format and clean `main` source and test files

--- src/main.cpp
+++ src/main.cpp
@@ -1,15 +1,6 @@
-#include "model/player/player.hpp"
-#include "model/position/position.hpp"
-
 #include <iostream>
 
 int main() {
-  Position position;
-  std::cout << "Position(row, column): " << position.row() << ","
-            << position.column() << std::endl;
-
-  Position position2(1, 2);
-  std::cout << "Position(row, column): " << position2.row() << ","
-            << position2.column() << std::endl;
-  return 0;
+    std::cout << "Hello, Chess Game!" << std::endl;
+    return 0;
 }
