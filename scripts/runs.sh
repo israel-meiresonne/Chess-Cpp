7981709007961eb082207655c8b77ebf6d25e3f0
Israel Meiresonne <israelmeiresonne97@gmail.com>
2025-08-13 19:34:08 +0000

    `scripts/runs.sh`: Refactor `run_install_libraries`

--- scripts/runs.sh
+++ scripts/runs.sh
@@ -56,59 +56,36 @@ run_formatter() {
   echo "$src_dir $tests_dir" | xargs clang-format -i
 }
 
-run_install_libs() {
+run_install_libraries() {
   if ! [ -z "$(jq empty $FILE_LOCKED_LIBS 2>&1)" ]; then
     echo "Error: '$FILE_LOCKED_LIBS' is invalid" >&2
     return 1
   fi
 
-  declare -A local_versions
-  for version_file in "$DIR_LIBS"/*/"$FILE_NAME_LIB_VERSION"; do
-      [[ -f "$version_file" ]] || continue
-      read -r version < "$version_file"
-      repo_name=$(basename "$(dirname "$version_file")")
-      local_versions[$repo_name]=$version
-  done
+  declare -A local_libraries && _assign_local_libraries
 
-  declare -A locked_versions
-  n_libs=$(jq 'length' "$FILE_LOCKED_LIBS")
-  for lib_i in {0..$((n_libs - 1))}; do
-      lib_url=$(jq -r --argjson i "$lib_i" '.[$i].url' "$FILE_LOCKED_LIBS")
-      version=$(jq -r --argjson i "$lib_i" '.[$i].version' "$FILE_LOCKED_LIBS")
-      repo_name=$(_github_repo_name "$lib_url")
-      locked_versions[$repo_name]=$version
-  done
+  declare -A locked_libraries && _assign_locked_libraries
 
-  for repo_name version in "${(@kv)local_versions}"; do
-    if ([[ -z "${locked_versions[$repo_name]}" ]]); then
-      echo "Library '$repo_name:$version' will be removed."
-      rm -fr "$DIR_LIBS/$repo_name"
-    elif ([[ "${locked_versions[$repo_name]}" != "$version" ]]); then
-      echo "Library '$repo_name:$version' will be replaced with '$repo_name:${locked_versions[$repo_name]}'."
-      rm -fr "$DIR_LIBS/$repo_name"
-    fi
-  done
+  _remove_invalid_libraries
 
-  for lib_i in {0..$((n_libs - 1))}; do
-    repo_url=$(jq -r --argjson i "$lib_i" '.[$i].url' "$FILE_LOCKED_LIBS")
-    branch=$(jq -r --argjson i "$lib_i" '.[$i].version' "$FILE_LOCKED_LIBS")
-    dir_repo_src_root=$(jq -r --argjson i "$lib_i" '.[$i].root' "$FILE_LOCKED_LIBS")
-
-    _install_lib "$repo_url" "$branch" "$dir_repo_src_root"
-  done
-}
-
-run_sync_libs() {
-  # Add and remove libs following the config file
-  echo 'Not implemented'
-  return 1
+  _install_libraries
 }
 
 ###############################################################################
 # Private functions                                                           #
 ###############################################################################
 
-_install_lib() {
+_install_libraries() {
+  for lib_i in {0..$((n_libs - 1))}; do
+    repo_url=$(jq -r --argjson i "$lib_i" '.[$i].url' "$FILE_LOCKED_LIBS")
+    branch=$(jq -r --argjson i "$lib_i" '.[$i].version' "$FILE_LOCKED_LIBS")
+    dir_repo_src_root=$(jq -r --argjson i "$lib_i" '.[$i].root' "$FILE_LOCKED_LIBS")
+
+    _install_library "$repo_url" "$branch" "$dir_repo_src_root"
+  done
+}
+
+_install_library() {
   repo_url="$1"
   branch="$2"
   dir_repo_src_root="$3"
@@ -146,6 +123,37 @@ _install_lib() {
   rm -fr "./$repo_name"
 
   echo "New library '$repo_name:$branch' has been successfully downloaded in '$dir_new_lib'."
+}
+
+_assign_local_libraries() {
+  for version_file in "$DIR_LIBS"/*/"$FILE_NAME_LIB_VERSION"; do
+    [[ -f "$version_file" ]] || continue
+    read -r version < "$version_file"
+    repo_name=$(basename "$(dirname "$version_file")")
+    local_libraries[$repo_name]=$version
+  done
+}
+
+_assign_locked_libraries() {
+  n_libs=$(jq 'length' "$FILE_LOCKED_LIBS")
+  for lib_i in {0..$((n_libs - 1))}; do
+    lib_url=$(jq -r --argjson i "$lib_i" '.[$i].url' "$FILE_LOCKED_LIBS")
+    version=$(jq -r --argjson i "$lib_i" '.[$i].version' "$FILE_LOCKED_LIBS")
+    repo_name=$(_github_repo_name "$lib_url")
+    locked_libraries[$repo_name]=$version
+  done
+}
+
+_remove_invalid_libraries() {
+  for repo_name version in "${(@kv)local_libraries}"; do
+    if ([[ -z "${locked_libraries[$repo_name]}" ]]); then
+      echo "Library '$repo_name:$version' will be removed."
+      rm -fr "$DIR_LIBS/$repo_name"
+    elif ([[ "${locked_libraries[$repo_name]}" != "$version" ]]); then
+      echo "Library '$repo_name:$version' will be replaced with '$repo_name:${locked_libraries[$repo_name]}'."
+      rm -fr "$DIR_LIBS/$repo_name"
+    fi
+  done
 }
 
 _clean_var() {
